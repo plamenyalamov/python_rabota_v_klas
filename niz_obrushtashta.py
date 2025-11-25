de6c93454676d874fd0a411d2fd383bfb1195ad9
Plamen Yalamov <plamenyalamov10@gmail.com>
2025-11-25 13:04:11 +0200

    iterativno

---
+++ niz_obrushtashta.py
@@ -0,0 +1,5 @@
+def reverse_str_iter(s):
+    result = ""
+    for ch in s:
+        result = ch + result
+    return result
---
+++ spisuk_chislo.py
@@ -0,0 +1,5 @@
+def sum_list_iter(nums):
+    total = 0
+    for n in nums:
+        total += n
+    return total
